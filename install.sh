1df759ad2cbbc8bb47d0566f4927dd898d46ca8c
JohnnyBra <retinadesdibujada@gmail.com>
2026-02-19 19:54:58 +0100

    Revert "Fix 'Unexpected end of JSON input' on Google Login"

--- install.sh
+++ install.sh
@@ -125,13 +125,6 @@ server {
     # Evita CORS desde el navegador y problemas de SSL en proxy nginx→HTTPS externo
     location ~ ^/api/(auth|prisma-users) {
         proxy_pass http://127.0.0.1:3011;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto https;
     }
 
     location / {
